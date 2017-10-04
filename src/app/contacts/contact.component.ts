14f00396389d8c0373ce79fd50f13a77ea8a4c75
Ashnita Bali <ashnita01@gmail.com>
2017-10-04 13:39:34 +0100

    Control Rx subscriptions with Async Pipe and BehaviorSubjects

--- src/app/contacts/contact.component.ts
+++ src/app/contacts/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Http } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/startWith';
@@ -16,18 +17,28 @@ import 'rxjs/add/operator/startWith';
   styles: []
 })
 export class ContactComponent implements OnInit {
-  contact$;
+  // BehaviorSubject starts with an initial value
+  // Instead of startWith we manually subscibe to the value
+  // The Observer of the stream is contact$
+  // Now we only have one subscription to the stream with http.get
+  // in it.
+  // We do have two subscriptions to this.contact$ because
+  // the BehaviorSubject is still an Observable
+  // but it is not going to make two requests because
+  // now it is just observing what comes from this stream 
+  // instead of invoking it twice. 
+  contact$ = new BehaviorSubject({name: 'loading...', image: ''});
 
   constructor(private route:ActivatedRoute, private http:Http) { 
     const api = 'https://starwars-json-server-ewtdxbyfdz.now.sh/';
     // params is an Observable
-    this.contact$ = route.params
+    route.params
       .map((p:any) => p.id)
       .switchMap(id => http.get(api + 'people/' + id)
         .map(res => res.json())
         .map(contact => Object.assign({}, contact, { image: api + contact.image }))
       )
-      .startWith({name: 'loading...'});
+      .subscribe(this.contact$);
   }
 
   ngOnInit() {
