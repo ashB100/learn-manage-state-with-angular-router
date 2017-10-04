cb80a4c58ee6387c8f22364f9ace7e9f8e296ff8
Ashnita Bali <ashnita01@gmail.com>
2017-10-04 13:23:00 +0100

    Lod data based on Angular Route Params

--- src/app/contacts/contact.component.ts
+++ src/app/contacts/contact.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/startWith';
 
 @Component({
   selector: 'app-contact',
   template: `
-    <p>
-      contact {{ id | async }}
-    </p>
+    <h2>
+      contact {{(contact$ | async)?.name}}
+    </h2>
+    <img [src]="(contact$ | async).image">
   `,
   styles: []
 })
 export class ContactComponent implements OnInit {
-  id;
-  
-  constructor(private route:ActivatedRoute) { 
+  contact$;
+
+  constructor(private route:ActivatedRoute, private http:Http) { 
+    const api = 'https://starwars-json-server-ewtdxbyfdz.now.sh/';
     // params is an Observable
-    this.id = route.params.map((p:any) => p.id);
+    this.contact$ = route.params
+      .map((p:any) => p.id)
+      .switchMap(id => http.get(api + 'people/' + id)
+        .map(res => res.json())
+        .map(contact => Object.assign({}, contact, { image: api + contact.image }))
+      )
+      .startWith({name: 'loading...'});
   }
 
   ngOnInit() {
